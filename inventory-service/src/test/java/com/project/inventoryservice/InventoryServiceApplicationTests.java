a749d1292f468f1b7a667cf10ef4febc558a78e2
bondev318 <daiquyen432@gmail.com>
2022-11-23 23:27:43 +0700

    inventory

--- inventory-service/src/test/java/com/project/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/project/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.customerservice;
+package com.project.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServiceApplicationTests {
+class InventoryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
