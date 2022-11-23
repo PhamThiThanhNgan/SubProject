d530d1d5be0eb24511e2d37eb389b53e2c6b7cfd
pham.thanhngan___ <phamthithanhngan@MacBook-Pro.local>
2022-11-23 23:03:56 +0700

    customerservice configserver

--- config-server/config-server/src/test/java/com/project/configserver/ConfigServerApplicationTests.java
+++ config-server/config-server/src/test/java/com/project/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.customerservice;
+package com.project.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
