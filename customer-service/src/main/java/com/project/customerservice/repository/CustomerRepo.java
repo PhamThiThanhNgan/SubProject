b91f5ad03c982a69a7120f439e4ed1c2c77b87d0
hophuocduy2610 <hophuocduy261020@gmail.com>
2022-11-25 01:52:04 +0700

    Finish API Gateway

--- customer-service/src/main/java/com/project/customerservice/repository/CustomerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.project.customerservice.repository;
-
-import com.project.customerservice.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Integer> {
-}
